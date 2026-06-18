4fbc8c1e18fc896edbf4e5f085d619aed82d3e17
Ravi kumar <ravvikumar272@gmail.com>
2026-06-18 13:47:46 +0530

    production domain added in the cors

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ai-interview-git-main-ravi-kumar195s-projects.vercel.app/",
+      "https://ai-interview-three-lime.vercel.app",
     ],
     credentials: true,
   }),
