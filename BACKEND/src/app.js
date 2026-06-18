7aecf7af78c1fc44caff462f6af420de6bbbff80
Ravi kumar <ravvikumar272@gmail.com>
2026-06-18 13:30:06 +0530

    cors link added

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-interview-git-main-ravi-kumar195s-projects.vercel.app/",
+    ],
     credentials: true,
   }),
 );
